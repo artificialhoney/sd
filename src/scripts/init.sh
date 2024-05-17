59de73cd7d36ad411c60620ea4f7f1344601fec0
Sebastian Krüger <sk@honeymachine.io>
2024-05-17 15:47:08 +0200

    fix: meta cmd

--- src/scripts/init.sh
+++ src/scripts/init.sh
@@ -4,6 +4,7 @@ export SD_SCRIPTS=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 set -a
 . "$SD_SCRIPTS/../projects/$SD_PROJECT.env"
+export SD_PROJECT_NAME="$SD_PROJECT"
 set +a
 
 export SD_OUTPUT="$SD_SCRIPTS/../../output/$SD_STYLE/$SD_PROJECT"
--- src/scripts/meta.sh
+++ src/scripts/meta.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-SD_META="$SD_OUTPUT/meta.txt"
+SD_META="$SD_OUTPUT/meta.yml"
 
 mkdir -p $SD_OUTPUT
 
 SD_TOP=4
 
-yake -ti "$SD_SETTING" -t $SD_TOP > "$SD_META"
-yake -ti "$SD_MUSE $SD_SETTING" -t $SD_TOP >> "$SD_META"
-yake -ti "$SD_OBJECT $SD_SETTING" -t $SD_TOP >> "$SD_META"
+cat >"$SD_META" <<-EOF
+---
+project: $SD_PROJECT_NAME
+style: $SD_STYLE
+bitter:
+    keywords: |
+$(yake -ti "$SD_SETTING" -t "$SD_TOP" | tail -4 | sed 's/^/        /')
+ginger:
+    keywords: |
+$(yake -ti "$SD_MUSE $SD_SETTING" -t "$SD_TOP" | tail -4 | sed 's/^/        /')
+joy:
+    keywords: |
+$(yake -ti "$SD_OBJECT $SD_SETTING" -t "$SD_TOP" | tail -4 | sed 's/^/        /')
+EOF
