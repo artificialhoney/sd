5a0e0c4067b742ef235685b1552172dfa698842b
Sebastian Krüger <sk@honeymachine.io>
2024-05-26 15:19:32 +0200

    feat: magical

--- src/scripts/init.sh
+++ src/scripts/init.sh
@@ -11,7 +11,8 @@ export SD_OUTPUT="$SD_SCRIPTS/../../output/$SD_STYLE/$SD_PROJECT"
 export SD_ASSETS="$SD_SCRIPTS/../../assets"
 
 export STYLED=${SD_STYLED:-giger/examples/styled/styled.py}
-export FACE=${SD_FACE:-"$SD_ASSETS/faces/palina/pantene.jpg"}
+export FACE=${SD_FACE:-"$SD_ASSETS/faces/palina/dirne.png"}
+export FACE_SIZE=${SD_FACE_SIZE:-640}
 export SIZE=${SD_SIZE:-10}
 export COUNT=${SD_COUNT:-4}
 export DIMENSION=${SD_DIMENSION:-"(960, 560)"}
